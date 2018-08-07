f410f78196324e768d397f7f58e41fb68a4a86c5
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-08-07 11:12:12 +0300

    create function for writing version to file

--- getv/main.py
+++ getv/main.py
@@ -1,8 +1,13 @@
+import os
 import subprocess
 
 GIT_COMMANDS = {
     "get_tag_list": "git tag -l",
 }
+
+PATH_TO_VIRTUAL_ENV = os.environ['VIRTUAL_ENV']
+
+PROJECT_DIR = os.path.dirname(PATH_TO_VIRTUAL_ENV)
 
 
 def run_shell_command(command, shell=True, stdout=subprocess.PIPE):
@@ -24,6 +29,14 @@ def get_last_version():
         return v_list[-1]
     return "No versions yet!"
 
+
+def write_or_create_version_file(path_to_dir, filename='version.txt'):
+    last_v = get_last_version()
+    with open(os.path.join(path_to_dir, filename), 'w') as f:
+        f.write(last_v)
+
+
+write_or_create_version_file(PROJECT_DIR)
 
 if __name__ == '__main__':
     last_v = get_last_version()
